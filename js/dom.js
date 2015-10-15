bc663dc3f2e5a3d202d72c8f9fec60608bb6a582
ethan <ethanmannette@gmail.com>
2015-10-15 11:28:30 -0600

    created new mouse move event

--- js/dom.js
+++ js/dom.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 
   // draw border
   drawBorder();
+  var pythag = false;
 
   // event listener for tree button
   $('#trees').click(function() {
@@ -15,11 +16,32 @@ $(document).ready(function() {
 
   });
 
-  $('#clear').click(function() {
-
-    clearCanvas();
-    drawBorder();
-
+  $('#pythag').click(function() {
+    Pythagoras();
+    pythag = true;
+    if (pythag) {
+      $('#canvas').mousemove(function(e) {
+        var page = {x: e.pageX, y: e.pageY};
+            Pythagoras.prototype.update({
+              x: page.x,
+              y: page.y
+            });
+      });
+    }
   });
 
+  $('#clear').click(function() {
+    clearCanvas();
+    drawBorder();
+    $('#canvas').off('mousemove');
+  });
+
+  // $('#canvas').mousemove(function(e) {
+  //   var elem_loc = this.getPosition();
+  // 			Pythagoras.prototype.update({
+  // 				x: e.page.x - elem_loc.x,
+  // 				y: e.page.y - elem_loc.y
+  //      });
+
+      // });
 });
